2ac01f7b7bfe78524b8d469795e3417e77201e6d
timhwtrosserw <benedikti909066252@rambler.ru>
2025-12-05 11:08:22 +0800

    add database-fragments.less, profile.rst, configuration-base.go

---
+++ frontend/web/src/complex/global-decorator-handler/example-handler.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\complex\global-decorator-handler\example-handler.rs
+// Generated at: 2025-10-25T16:07:01.765Z
---
+++ spec-product/middleware-local-api/profile.rst
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\spec-product\middleware-local-api\profile.rst
+// Generated at: 2025-10-25T16:07:01.740Z
